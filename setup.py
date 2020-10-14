5e869a1262ad601aaa1e4c595a428dd74c5d3175
Austin Bingham <austin.bingham@gmail.com>
2020-10-14 15:38:06 +0200

    Using only setup.py, not setup.cfg.

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
 from setuptools import setup
 
-setup()
+setup(
+    name="xontrib-vox-completion",
+    version="0.1",
+    license="MIT",
+    url="https://github.com/abingham/xontrib-vox-completion",
+    description="Command completion for the vox command in xonsh",
+    author="Austin Bingham",
+    author_email="austin.bingham@gmail.com",
+    packages=['xontrib'],
+    package_dir={'xontrib': 'xontrib'},
+    package_data={'xontrib': ['*.xsh']},
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
